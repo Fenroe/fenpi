521890e80aba1753dad536ed65b3b2369f9c18c2
Fenroe <scrambles9494@gmail.com>
2022-07-23 05:11:15 +0300

    finished build

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
 app.use('/api', routes_1.default);
 app.listen(8080, () => {
     console.log('The application is listening on port 8080');
