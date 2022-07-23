0a7a7b62212d9fd0cba15c4993b93b5d3db0b0b2
Fenroe <scrambles9494@gmail.com>
2022-07-23 05:09:50 +0300

    added cors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 import apiRouter from './routes'
 
 dotenv.config()
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 app.use('/api', apiRouter)
 
 app.listen(8080, () => {
