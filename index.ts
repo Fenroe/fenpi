b2269e58c7254726fca52787652162b664bb2c8e
fenroe <scrambles9494@gmail.com>
2022-11-03 16:36:38 +0200

    integrated mongodb into app

--- index.ts
+++ index.ts
@@ -2,10 +2,19 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import apiRouter from './routes'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const app = express()
+
+const mongoCredentials = process.env.MONGODB_CREDENTIALS!
+
+mongoose.connect(mongoCredentials)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
