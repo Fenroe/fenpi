697243c597546a34c29d65e4b6b89ee8b11568b5
fenroe <scrambles9494@gmail.com>
2022-11-07 17:35:02 +0200

    rebuilt

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
+const compression_1 = __importDefault(require("compression"));
+const helmet_1 = __importDefault(require("helmet"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const corsOptions = {
     origin: process.env.CORS_ORIGIN
 };
+app.use((0, compression_1.default)());
+app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)(corsOptions));
 app.use('/api', routes_1.default);
-app.listen(8080, () => {
-    console.log('The application is listening on port 8080');
+app.listen(process.env.PORT, () => {
+    console.log('The application is listening on port' + process.env.PORT);
 });
