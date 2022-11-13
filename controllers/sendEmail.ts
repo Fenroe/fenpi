3817c3e09355b4dd8c9766cca358bdedf31ffe40
fenroe <scrambles9494@gmail.com>
2022-11-13 02:56:56 +0200

    removed some console logs and made minor changes

--- controllers/sendEmail.ts
+++ controllers/sendEmail.ts
@@ -10,14 +10,13 @@ const sendEmail = [
   body('text').isString().notEmpty(),
   async (req: any, res: any, next: Function) => {
     try {
-      console.log(req.body)
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         console.log(errors)
         throw new Error('Validation failed')
       }
       const { topic, name, emailAddress, heading, text } = req.body
-      const origin = req.get('host')
+      const origin = req.get('origin')
       const messageSubject = getMessageSubject(origin)
       const messagePlaintext = getMessagePlaintext(topic, name, emailAddress, heading, text, origin)
       const messageHtml = getMessageHtml(topic, name, emailAddress, heading, text, origin)
@@ -38,10 +37,7 @@ const sendEmail = [
         logger: true
       }
       const transporter = createTransport(transport)
-      console.log({ user: process.env.TRANSPORT_USER, pass: process.env.TRANSPORT_PASSWORD })
-      console.log('before')
       await transporter.sendMail(message)
-      console.log('after')
       return res.status(201).send({ message: 'Email was sent' })
     } catch (err) {
       return next(err)
