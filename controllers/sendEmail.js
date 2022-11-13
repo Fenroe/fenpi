7fd29bee82e447633aa1123388447c914b97966f
fenroe <scrambles9494@gmail.com>
2022-11-13 03:08:05 +0200

    rebuilt with previous changes

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -15,19 +15,18 @@ const nodemailer_1 = require("nodemailer");
 const sendEmail = [
     (0, express_validator_1.body)('topic').isString().notEmpty(),
     (0, express_validator_1.body)('name').isString(),
-    (0, express_validator_1.body)('emailAddress').isEmail(),
+    (0, express_validator_1.body)('emailAddress').isString(),
     (0, express_validator_1.body)('heading').isString(),
     (0, express_validator_1.body)('text').isString().notEmpty(),
     (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            console.log(req.body);
             const errors = (0, express_validator_1.validationResult)(req);
             if (!errors.isEmpty()) {
                 console.log(errors);
                 throw new Error('Validation failed');
             }
             const { topic, name, emailAddress, heading, text } = req.body;
-            const origin = req.get('host');
+            const origin = req.get('origin');
             const messageSubject = (0, utils_1.getMessageSubject)(origin);
             const messagePlaintext = (0, utils_1.getMessagePlaintext)(topic, name, emailAddress, heading, text, origin);
             const messageHtml = (0, utils_1.getMessageHtml)(topic, name, emailAddress, heading, text, origin);
@@ -48,10 +47,7 @@ const sendEmail = [
                 logger: true
             };
             const transporter = (0, nodemailer_1.createTransport)(transport);
-            console.log({ user: process.env.TRANSPORT_USER, pass: process.env.TRANSPORT_PASSWORD });
-            console.log('before');
             yield transporter.sendMail(message);
-            console.log('after');
             return res.status(201).send({ message: 'Email was sent' });
         }
         catch (err) {
